ce9c84a9fb80859fcdadc2c87dccc2c9b8ddb924
everdimension <everdimension@gmail.com>
2022-05-19 17:05:06 +0300

    Update typings

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export function capitalize<T extends string>(value: string): Capitalize<T> {
+export function capitalize<T extends string>(value: T): Capitalize<T> {
   const result = value.charAt(0).toUpperCase() + value.slice(1);
   return result as Capitalize<T>;
 }
